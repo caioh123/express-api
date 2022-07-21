c7f69ead7eb4d985c0fed12acffb539b507fc0c4
Caio Novaes <caionwebdev@gmail.com.br>
2022-07-20 22:16:45 -0300

    add space

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
-
+ 
 app.get("/", (req, res) => {
   res.render("index", {
     title: "caio henrique",
